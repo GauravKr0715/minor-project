aa57791e8d19c00a259642f0b6841b8b32ce6f68
pooja906 <poojav9810@gmail.com>
2021-09-30 18:39:56 +0530

    add Student Login

--- client/src/components/Login/S_Login.js
+++ client/src/components/Login/S_Login.js
@@ -1,31 +1,114 @@
-import React, { Component } from 'react';
+import React from "react";
+import "./Login.css";
+import logo from "/Users/pooja/Desktop/M_Project/minor-project/client/src/assets/image/s.png";
+import col_logo from "/Users/pooja/Desktop/M_Project/minor-project/client/src/assets/image/logo.jpg"
+// import Slogo from "/Users/pooja/Desktop/M_Project/minor-project/client/src/assets/image/s_logo.jpeg"
+// import { Link } from "react-router-dom";
+// import image1 from "../assets/image/s.png";
 
-class StudentLogin extends Component {
-    render() {
-        return (
-            <div className="base-container">
-                <div className="header">Login</div>
-                <div className="content">
-                    <div className="image">
-                        <img src="../assets/image/s.png" alt="ui-element"/>
-                    </div>
-                    <div className="form">
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" name="username" placeholder="username" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder="password" />
-                        </div>
-                    </div>
-                </div>
-                <div className="footer">
-                    <button type="submit" className="btn">Login</button>
-                </div>
-            </div>
-        );
-    }
-}
+export default function StudentLogin() {
+  // const [values, setValues] = useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   enrollNo: "",
+  //   email: "",
+  //   password: "",
+ 
 
-export default StudentLogin;
+ 
+
+    return (
+  
+        <div>
+          <div className="top-nav">
+            
+            <img className="col_logo" src={col_logo} alt="" />
+          </div>
+
+      <div class="logo">
+      <img className="icon" src={logo} alt="" />
+      </div>  
+    
+      
+           
+      
+      <div class="form-container">
+      
+        
+
+           {/* <div > */}
+            <h1>STUDENT LOGIN</h1>
+        
+          {/* <img src="../assets/image/a.png" alt="ui-element"/> */}
+        <form class="register-form">
+          {/* <div className="success-message"></div> */}
+          {/* Uncomment the next line to show the success message */}
+          {/* <div class="success-message">Success! Thank you for registering</div> */}
+          {/* <h6>first</h6> */}
+          {/* <label for="fname">First name: </label>
+          <input
+            // onChange = {handleFirstNameInputchange}
+            // value={values.firstName}
+            id="first-name"
+            class="form-field"
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+          />
+          {/* Uncomment the next line to show the error message */}
+          {/* <span id="first-name-error">Please enter a first name</span> */}
+          {/* <label for="fname">Last name: </label>
+          <input
+            // onChange = {handleLastNameInputchange}
+            // value={values.lastName}
+            id="last-name"
+            class="form-field"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+          /> */} 
+          {/* Uncomment the next line to show the error message */}
+          {/* <span id="last-name-error">Please enter a last name</span> */}
+          <label for="fname">Enrollment Number: </label>
+          <input
+            // onChange = {handleEnrollNoInputchange}
+            // value={values.enrollNo}
+            id="enroll-no"
+            class="form-field"
+            type="text"
+            placeholder="Enrollment No"
+            name="enrollNo"
+          />
+          {/* <label for="fname">Email : </label>
+
+          <input */}
+            {/* // onChange = {handleEmailInputchange}
+            // value={values.email}
+          //   id="email"
+          //   class="form-field"
+          //   type="text"
+          //   placeholder="Email"
+          //   name="email"
+          // /> */}
+          <label for="fname">Password : </label>
+
+          <input
+            // onChange = {handlePasswordInputchange}
+            // value={values.password}
+            id="password"
+            class="form-field"
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          {/* Uncomment the next line to show the error message */}
+          {/* <span id="email-error">Please enter an email address</span> */}
+          <button class="button" type="submit">
+            LOGIN
+          </button>
+          
+        </form>
+      </div>
+      </div>
+    );
+  }
